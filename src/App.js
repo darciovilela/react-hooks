93154e08795e47466bf288907b2b4bb71142e672
Darcio Vilela <darcio@jp7.com.br>
2022-01-31 15:44:01 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import EffectTutorial from './EffectTutorial';
 import ReducerTutorial from './ReducerTutorial';
 import StateTutorial from './StateTutorial';
 import StateTutorialNew from './StateTutorialNew';
@@ -7,10 +8,15 @@ const App = () => {
   return (
     <div>
       <StateTutorial />
+      <br></br>
       <hr></hr>
       <StateTutorialNew />
+      <br></br>
       <hr></hr>
       <ReducerTutorial />
+      <br></br>
+      <hr></hr>
+      <EffectTutorial />
     </div>
   );
 };
