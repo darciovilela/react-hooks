e05c00ccf283c55976b9b4f0b66275109039843d
Darcio Vilela <darcio@jp7.com.br>
2022-02-04 17:47:41 -0300

    Changing inline style to .css file

--- src/StateTutorialNew.js
+++ src/StateTutorialNew.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 // keep track and re-render the page if state changes
 
 const initialName = () => {
-  return <h3 style={{ color: 'blue' }}>My name is Darcio</h3>;
+  return <h4>My name is Darcio</h4>;
 };
 
 const StateTutorialNew = () => {
@@ -11,14 +11,14 @@ const StateTutorialNew = () => {
 
   const onChange = (event) => {
     const secName = event.target.value;
-    const newValue = <h3 style={{ color: 'blue' }}>{secName}</h3>;
+    const newValue = <h4>{secName}</h4>;
     setInputValue(newValue);
   };
 
   return (
     <div>
       <h2>useState - Sample 2</h2>
-      <input placeholder="Digit some text..." onChange={onChange} />
+      <input placeholder="Digit your name..." onChange={onChange} />
       {inputValue}
     </div>
   );
